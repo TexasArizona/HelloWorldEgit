17cc01534dad84b8ea00991abca919b400a9c54e
James Bond <ns20092009@live.de>
2017-01-11 14:22:06 +0100

    Added one loop to

--- src/main/java/com/git/example/HelloWorld.java
+++ src/main/java/com/git/example/HelloWorld.java
@@ -1,18 +1,21 @@
 package com.git.example;
 
 public class HelloWorld {
-	
+
 	public static void main(String[] args) {
-		
+
 		int i;
+		int j;
 		System.out.println("Hello World");
-		
-		for(i = 0; i < 5; i ++){
+
+		for (i = 0; i < 5; i++) {
+			System.out.println("Hello World once again");
+		}
+
+		for (j = 0; j < 5; j++) {
 			System.out.println("Hello World once again");
 		}
 
 	}
-	
-	
 
 }
