ca1624856b807dfb79d9f2dc63686a0e05985737
James Bond <ns20092009@live.de>
2017-01-11 13:49:25 +0100

    Added for loop
    Change-Id: Id67b20e87261d6ee87123be6463011d6acba97f9
    Signed-off-by: James Bond <ns20092009@live.de>

--- src/main/java/com/git/example/HelloWorld.java
+++ src/main/java/com/git/example/HelloWorld.java
@@ -1,10 +1,18 @@
 package com.git.example;
 
 public class HelloWorld {
-
+	
 	public static void main(String[] args) {
+		
+		int i;
 		System.out.println("Hello World");
+		
+		for(i = 0; i < 5; i ++){
+			System.out.println("Hello World once again");
+		}
 
 	}
+	
+	
 
 }
